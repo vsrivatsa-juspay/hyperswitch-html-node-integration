a64825efc9a5bb2af742f7ecf9e0291e122360f3
Kritik Modi <kritik.modi@juspay.in>
2023-07-19 16:16:42 +0530

    Add files

---
+++ public/script.js
@@ -0,0 +1,145 @@
+//Follow this doc for HTML and REST API integration: https://hyperswitch.io/docs/sdkIntegrations/unifiedCheckoutWeb/restAPIBackendAndHTMLFrontend
+// Update your publishable key here
+
+const hyper = Hyper("pk_snd_3b33cd9404234113804aa1accaabe22f");
+
+const items = [{ id: "xl-tshirt" }];
+
+async function initialize() {
+  // For demo purpose we are calling the hyperswitch API directly here, but in actual setup this should be backend call to your server and call the hyperswitch API internally with your API key
+  const response = await fetch("/create-payment", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ items }),
+  });
+  const {
+    client_secret
+  } = await response.json();
+
+  // You can change the apperance of the SDK by adding field here
+  const appearance = {
+    // theme: "midnight",
+  };
+
+  widgets = hyper.widgets({
+    appearance,
+    clientSecret: client_secret
+  });
+
+  const unifiedCheckoutOptions = {
+    layout: "tabs",
+    wallets: {
+      walletReturnUrl: "https://example.com/complete",
+      //Mandatory parameter for Wallet Flows such as Googlepay, Paypal and Applepay
+    },
+  };
+
+  const unifiedCheckout = widgets.create("payment", unifiedCheckoutOptions);
+  unifiedCheckout.mount("#unified-checkout");
+}
+initialize();
+
+async function handleSubmit(e) {
+  setLoading(true);
+  const { error, data, status } = await hyper.confirmPayment({
+    widgets,
+    confirmParams: {
+      // Make sure to change this to your payment completion page
+      return_url: "https://example.com/complete",
+    },
+  });
+
+  // This point will only be reached if there is an immediate error occurring while confirming the payment. Otherwise, your customer will be redirected to your `return_url`.
+
+  // For some payment flows such as Sofort, iDEAL, your customer will be redirected to an intermediate page to complete authorization of the payment, and then redirected to the `return_url`.
+
+  if (error && error.type === "validation_error") {
+    showMessage(error.message);
+  }
+  else if (status === "succeeded") {
+    addClass("#hypers-sdk", "hidden")
+    removeClass("#orderSuccess", "hidden")
+  }
+  else {
+    showMessage("An unexpected error occurred.");
+  }
+
+  setLoading(false);
+}
+
+// Fetches the payment status after payment submission
+async function checkStatus() {
+  const clientSecret = new URLSearchParams(window.location.search).get(
+    "payment_intent_client_secret"
+  );
+
+  if (!clientSecret) {
+    return;
+  }
+
+  const { payment } = await hyper.retrievePayment(clientSecret);
+
+  switch (payment.status) {
+    case "succeeded":
+      showMessage("Payment succeeded!");
+      break;
+    case "processing":
+      showMessage("Your payment is processing.");
+      break;
+    case "requires_payment_method":
+      showMessage("Your payment was not successful, please try again.");
+      break;
+    default:
+      showMessage("Something went wrong.");
+      break;
+  }
+}
+
+function setLoading(showLoader) {
+  if (showLoader) {
+    show('.spinner');
+    hide('#button-text');
+  }
+  else {
+    hide('.spinner');
+    show('#button-text');
+  }
+}
+
+function show(id) {
+  removeClass(id, 'hidden');
+}
+function hide(id) {
+  addClass(id, 'hidden');
+}
+
+function showMessage(msg) {
+  show('#payment-message');
+  addText('#payment-message', msg);
+}
+
+function addText(id, msg) {
+  var element = document.querySelector(id);
+  element.innerText = msg;
+}
+
+function addClass(id, className) {
+  var element = document.querySelector(id);
+  element.classList.add(className);
+}
+
+function removeClass(id, className) {
+  var element = document.querySelector(id);
+  element.classList.remove(className);
+}
+
+function retryPayment() {
+  hide('#orderSuccess');
+  show('.Container');
+  initialize();
+}
+
+function showSDK(e) {
+  hide('.Container');
+  show('#hypers-sdk');
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const hyperswitch = require("@juspay-tech/hyperswitch-node")('snd_c691ade6995743bd88c166ba509ff5da');
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const calculateOrderAmount = (items) => {
+  return 1345;
+};
+
+app.post("/create-payment", async (req, res) => {
+
+  const { items } = req.body;
+
+  const paymentIntent = await hyperswitch.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "USD",
+    business_label: "default",
+    business_country: "US",
+  });
+
+  res.send({
+    client_secret: paymentIntent.client_secret,
+  });
+});
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
