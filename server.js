746c4f098b3b629818dab366412258bf05824883
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-07-31 14:43:25 +0530

    Remove business_country and business_label and add comment

--- server.js
+++ server.js
@@ -14,11 +14,16 @@ app.post("/create-payment", async (req, res) => {
 
   const { items } = req.body;
 
+  /*
+     If you have two or more “business_country” + “business_label” pairs configured in your Hyperswitch dashboard,
+     please pass the fields business_country and business_label in this request body.
+     For accessing more features, you can check out the request body schema for payments-create API here :
+     https://api-reference.hyperswitch.io/docs/hyperswitch-api-reference/60bae82472db8-payments-create
+  */
+
   const paymentIntent = await hyperswitch.paymentIntents.create({
     amount: calculateOrderAmount(items),
     currency: "USD",
-    business_label: "default",
-    business_country: "US",
   });
 
   res.send({
